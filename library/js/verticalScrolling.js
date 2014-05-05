13b69ce015c9b0f12dd23c87ac40f5c8c6d013c4
Arabirim <aycan.bulbul@arabirim.com.tr>
2014-05-05 09:52:25 +0300

    Update verticalScrolling.js

--- library/js/verticalScrolling.js
+++ library/js/verticalScrolling.js
@@ -1,21 +1,20 @@
-if ( !window.idx )
-{
-    jQuery("html").scrollTop(1); var hst = jQuery("html").scrollTop()*1;
-    jQuery("html").scrollTop(0); window.idx = hst !== 1 ? "body" : "html";    
-}
-
-function verticalScrolling() {
-    "use strict";
+function verticalScrolling() { "use strict";
     jQuery('[data-target]').on('click', function (e) 
     {
         e.preventDefault();
 
         var object = jQuery(this);
-        var destination = object.attr('data-target');
-        var effect      = object.attr('data-effect');
-        var time        = object.attr('data-time');
-        var callback    = object.attr('data-callback');
-
+        var destination         = object.attr('data-target');
+        var effect              = object.attr('data-effect');
+        var time                = object.attr('data-time');
+        var dataAnimateBefore   = object.attr('data-animate-before');
+        var dataAnimateComplete = object.attr('data-animate-complete');
+        
+        if( dataAnimateBefore )
+        {
+            new Function( dataAnimateBefore ) ();   
+        }
+        
         // destination control
         if (destination.search(/^[\.#]/gi) > -1) {
             // object
@@ -36,7 +35,7 @@ function verticalScrolling() {
             'easeOutCirc', 'easeInOutCirc', 'easeInElastic', 'easeOutElastic', 'easeOutElastic',
             'easeInBack', 'easeOutBack', 'easeInOutBack', 'easeInBounce', 'easeOutBounce', 'easeInOutBounce'
         ];
-
+        
         if (destination > -1) {
             var value = destination;
             jQuery('.activePage').removeClass('activePage');
@@ -45,8 +44,9 @@ function verticalScrolling() {
             jQuery('.activePage').removeClass('activePage');
             destination.addClass('activePage');
         }
+        var i = 0;
         
-        jQuery( window.idx ).stop( true,false ).animate
+        jQuery( 'html,body' ).finish().animate
         (
             {
                 scrollTop: value
@@ -54,15 +54,18 @@ function verticalScrolling() {
             time || 600,
             effect === 'random' ?  effects[Math.floor((Math.random() * (effects.length)) + 1)] : effects[0],
             function () {
-                eval(callback);
+                if(i === 1)
+                    if( dataAnimateComplete )
+                        new Function( dataAnimateComplete )();
+                i++;
             }
         );
     });
 
     jQuery(window).resize(function ()
     {
-        jQuery( 'body' ).stop( true,false );
-        jQuery( 'body' ).scrollTop( jQuery( '.activePage' ).offset().top );
+        jQuery( 'html,body' ).finish();
+        jQuery( 'html,body' ).scrollTop( jQuery( '.activePage' ).offset().top );
     });
 }
 
